05618b0a97d5fce4a028e6dc7ecf2f8bd0ed9a6c
Håkon Hjortdahl <haakohjo@ntnu.no>
2023-10-12 10:26:42 +0200

    initial commit

---
+++ node_2/main.c
@@ -0,0 +1,21 @@
+/*
+ * node2.c
+ *
+ * Created: 12.10.2023 10:15:50
+ * Author : haakohjo
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
